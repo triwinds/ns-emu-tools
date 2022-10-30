8ab286b80bcd27b77ae1081f289931e78860e26d
triwinds <triwinds@users.noreply.github.com>
2022-10-30 21:47:02 +0800

    update log file name

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s.%(msecs)03d|%(levelname)s|%(name)s|%(filename)s:%(lineno)s|%(funcName)s|%(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    handlers=[RotatingFileHandler('yuzu-tools.log', encoding='utf-8', maxBytes=10 * 1024 * 1024, backupCount=10),
+    handlers=[RotatingFileHandler('ns-emu-tools.log', encoding='utf-8', maxBytes=10 * 1024 * 1024, backupCount=10),
               console]
 )
 logger = logging.getLogger(__name__)
