c1e3c4920ebec5164dfb5eaad6aef8e96aa7a005
triwinds <triwinds@users.noreply.github.com>
2022-11-03 19:51:30 +0800

    update pyinstaller command

--- package.bat
+++ package.bat
@@ -3,6 +3,6 @@ chcp>nul 2>nul 65001
 :path
 cd>nul 2>nul /D %~dp0
 call venv\Scripts\activate.bat
-pyinstaller --noconfirm --onefile --windowed --icon "./web/favicon.ico" --add-data "./module/aria2c.exe;./module/" --add-data "./web;web/"  "./main.py" --name "NsEmuTools"
-rem pyinstaller --noconfirm --onefile --console --icon "./web/favicon.ico" --add-data "./module/aria2c.exe;./module/" --add-data "./web;web/"  "./main.py" --name "NsEmuTools"
+pyinstaller --noconfirm --onefile --windowed --icon "./web/favicon.ico" --add-data "./module/*.exe;./module/" --add-data "./web;web/"  "./main.py" --name "NsEmuTools"
+rem pyinstaller --noconfirm --onefile --console --icon "./web/favicon.ico" --add-data "./module/*.exe;./module/" --add-data "./web;web/"  "./main.py" --name "NsEmuTools"
 pause
