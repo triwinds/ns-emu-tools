1373760937b3c1c89ea4e4457b5248cc9c21b904
triwinds <triwinds@users.noreply.github.com>
2022-11-11 01:27:14 +0800

    update terser config

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,7 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  outputDir: "../web"
+    transpileDependencies: [
+        'vuetify'
+    ],
+    outputDir: "../web",
+    productionSourceMap: false,
+    chainWebpack: config => {
+        // config.optimization.minimize(false)
+        config.optimization.minimizer('terser').tap(options => {
+            let option = options[0]
+            option.exclude = /app/
+            return options
+        })
+    }
 })
