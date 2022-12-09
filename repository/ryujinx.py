c308e57c6b472a4473a8fd03af425187fc7fdbd1
triwinds <triwinds@users.noreply.github.com>
2022-12-09 22:44:18 +0800

    add button to show Ryujinx change log

--- repository/ryujinx.py
+++ repository/ryujinx.py
@@ -1,4 +1,4 @@
-from utils.network import request_github_api
+from utils.network import request_github_api, session, get_finial_url
 
 
 def get_all_ryujinx_release_infos():
@@ -11,3 +11,8 @@ def get_latest_ryujinx_release_info():
 
 def get_ryujinx_release_info_by_version(version):
     return request_github_api(f'https://api.github.com/repos/Ryujinx/release-channel-master/releases/tags/{version}')
+
+
+def load_ryujinx_change_log():
+    resp = session.get(get_finial_url('https://raw.githubusercontent.com/wiki/Ryujinx/Ryujinx/Changelog.md'))
+    return resp.text
