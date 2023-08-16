e7f47e997e0425cb0bef84c9fc60593424e571bb
triwinds <triwinds@users.noreply.github.com>
2023-08-17 01:15:17 +0800

    add roboto font

--- vue3/src/plugins/webfontloader.ts
+++ vue3/src/plugins/webfontloader.ts
@@ -4,16 +4,17 @@
  * webfontloader documentation: https://github.com/typekit/webfontloader
  */
 import '@fontsource-variable/jetbrains-mono'
+import '@fontsource/roboto';
 
 export async function loadFonts () {
   const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
 
   webFontLoader.load({
     // google: {
-    //   families: ['JetBrains Mono'],
+    //   families: ['Roboto:100,300,400,500,700,900&display=swap'],
     // },
     custom: {
-      families: ['JetBrains Mono Variable']
+      families: ['JetBrains Mono Variable', 'Roboto'],
     }
   })
 }
