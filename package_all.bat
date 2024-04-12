f4c40da3e96ff4cdeb9665e5e77acbe9f1c26f6a
triwinds <triwinds@users.noreply.github.com>
2024-04-12 22:46:58 +0800

    add zip_files.py

--- package_all.bat
+++ package_all.bat
@@ -6,4 +6,5 @@ rem call venv\Scripts\activate.bat
 poetry run pyinstaller --noconfirm --onefile --windowed --icon "./web/favicon.ico" --add-data "./module/*.exe;./module/" --add-data "./web;web/"  "./main_devnull.py" --additional-hooks-dir=".\\hooks" --name "NsEmuTools"
 poetry run pyinstaller --noconfirm --onefile --console --icon "./web/favicon.ico" --add-data "./module/*.exe;./module/" --add-data "./web;web/"  "./main.py" --additional-hooks-dir=".\\hooks" --name "NsEmuTools-console"
 poetry run pyinstaller --noconfirm --windowed --icon "./web/favicon.ico" --add-data "./module/*.exe;./module/" --add-data "./web;web/"  "./main_devnull.py" --additional-hooks-dir=".\\hooks" --name "NsEmuTools"
+poetry run python build_tools/zip_files.py
 pause
