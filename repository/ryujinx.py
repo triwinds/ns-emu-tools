ef2d18517a43552e70a58a462a617c48535a0307
triwinds <triwinds@users.noreply.github.com>
2025-03-15 11:23:15 +0800

    refactor(ryujinx): 更新代码以适应项目迁移
    -项目已从 GitHub 迁移到 GitLab，更新相关 API URL
    - 新增注释说明新的项目位置和 API
    - 修改 release 信息获取函数，以适应新的 API 结构

--- repository/ryujinx.py
+++ repository/ryujinx.py
@@ -1,10 +1,14 @@
 from module.network import request_github_api, session, get_finial_url
 
 
+# They move them codes to https://git.ryujinx.app/ryubing/ryujinx
+# the api of releases https://git.ryujinx.app/api/v4/projects/1/releases  (using GitLab)
+
+
 def get_all_ryujinx_release_infos(branch='mainline'):
     if branch == 'canary':
         return get_all_canary_ryujinx_release_infos()
-    return request_github_api('https://api.github.com/repos/Ryubing/Ryujinx/releases')
+    return request_github_api('https://api.github.com/repos/Ryubing/Stable-Releases/releases')
 
 
 def get_all_canary_ryujinx_release_infos():
@@ -18,7 +22,7 @@ def get_latest_ryujinx_release_info():
 def get_ryujinx_release_info_by_version(version, branch='mainline'):
     if branch == 'canary':
         return get_canary_ryujinx_release_info_by_version(version)
-    return request_github_api(f'https://api.github.com/repos/Ryubing/Ryujinx/releases/tags/{version}')
+    return request_github_api(f'https://api.github.com/repos/Ryubing/Stable-Releases/releases/tags/{version}')
 
 
 def get_canary_ryujinx_release_info_by_version(version):
