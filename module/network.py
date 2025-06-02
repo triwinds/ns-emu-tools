270e0f065f946360c566d1427401f10f27516389
triwinds <triwinds@users.noreply.github.com>
2025-06-02 13:26:30 +0800

    update UA

--- module/network.py
+++ module/network.py
@@ -112,8 +112,8 @@ options_on_cdn = {
     'min-split-size': '12M',
 }
 
-chrome_ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-            'Chrome/113.0.0.0 Safari/537.36'
+chrome_ua = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+             'Chrome/136.0.0.0 Safari/537.36')
 github_api_fallback_flag = False
 
 
