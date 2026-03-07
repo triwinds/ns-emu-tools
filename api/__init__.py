0369a473de7222f818cb8637b0b6479a2c3b8ee9
triwinds <triwinds@users.noreply.github.com>
2026-03-07 15:11:23 +0800

    clean up python codes

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import pkgutil
-from importlib import import_module
-
-__all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
-
-for m in __all__:
-    import_module(f'.{m}', 'api')
--- exception/install_exception.py
+++
@@ -1,8 +0,0 @@
-class FailToCopyFiles(Exception):
-    raw_exception: Exception
-    msg: str
-
-    def __init__(self, raw_exception: Exception, msg):
-        self.raw_exception = raw_exception
-        self.msg = msg
-        super().__init__(msg)
